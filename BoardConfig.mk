53bd39a22e87f6a9a506cfa462522351ece2cd13
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-11 06:54:25 +0530

    woods: Initial tree

---
+++ BoardConfig.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# inherit from common mt6737-common
+-include device/motorola/mt6737-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/motorola/woods
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Moto_E4,Moto E4,moto_e4,e4,woods,woods_f
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/motorola/woods
+TARGET_KERNEL_CONFIG := woods_defconfig
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# inherit from the proprietary version
+-include vendor/motorola/woods/BoardConfigVendor.mk
